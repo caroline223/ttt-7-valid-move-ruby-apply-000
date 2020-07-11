7799b04877d2b0fc4ae9d8f9499cf0deb4ac3264
Caroline Forrester <carolineforrester22@gmail.com>
2020-07-11 23:28:22 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -8,13 +8,14 @@ def valid_move?(board, index)
     end
   end
   
-  def position_taken?(board, index)
-    if board[index] == ""  || board[index] == " " || board[index] == nil
-      return false
-    else
-      return true 
-    end
-  end
+board = [" "," "," "," "," "," "," "," "," "]
+def position_taken?(board,index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else
+    return true 
+end
+end
   
   
 end
